b0fdd560002f6e69590c524adc33ec84994b18da
Michael Hernandez <michael.hernandez1988@gmail.com>
2017-11-09 11:48:14 -0500

    added redux-observable

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -4,8 +4,14 @@ import { createStore, applyMiddleware, combineReducers } from '../lib/rx-redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import * as reducers from '../reducers';
+import { createEpicMiddleware } from 'redux-observable';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { rootEpic } from '../epics';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+// We should think of refactoring apply middleware to be an argument of createStore
+
+const epicMiddleware = createEpicMiddleware(rootEpic);
+const createStoreWithMiddleware = applyMiddleware(thunk, epicMiddleware)(createStore);
 const reducer = combineReducers(reducers);
 const store = createStoreWithMiddleware(reducer);
 
---
+++ frontend/src/epics/index.js
@@ -0,0 +1,15 @@
+import Rx from 'rxjs';
+import { Observable } from 'rxjs/Observable';
+import { combineEpics } from 'redux-observable';
+import { INCREMENT_COUNTER, DECREMENT_COUNTER } from '../constants/ActionTypes';
+
+export const incrementEpic = action$ => {
+  console.log(action$);
+  return action$.filter(action => action.type === INCREMENT_COUNTER)
+    .mapTo({ type: DECREMENT_COUNTER });
+}
+
+
+export const rootEpic = combineEpics(
+  incrementEpic,
+);
