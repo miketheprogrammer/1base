1dd4ca5a0973985c2b3225c46b6ba557f6341ce2
Michael Hernandez <michael.hernandez1988@gmail.com>
2017-11-12 10:58:28 -0500

    basic schemas and indexes

--- backend/schemas/mongoose/index.js
+++ backend/schemas/mongoose/index.js
@@ -1,8 +1,10 @@
-var mongoose    = require('mongoose');
-var Schema      = mongoose.Schema;
+const mongoose    = require('mongoose');
+const Schema      = mongoose.Schema;
 
+const ObjectId = mongoose.Schema.Types.ObjectId;
+const Mixed = mongoose.Schema.Types.Mixed;
 
-var Counter = exports.Counter = new Schema({
+const Counter = exports.Counter = new Schema({
   name: {
     type: String,
     index: true,
@@ -11,7 +13,7 @@ var Counter = exports.Counter = new Schema({
   counter:  Number,
 });
 
-var User = exports.User = new Schema({
+const User = exports.User = new Schema({
   username: {
     type: String,
     index: true,
@@ -20,3 +22,56 @@ var User = exports.User = new Schema({
   password:  String,
   created: Date
 });
+
+const Organization = exports.Organization = new Schema({
+  name: {
+    type: String,
+    index: true,
+    unique: true
+  },
+  owner: String,
+  members: [String],
+});
+
+const Game = exports.Game = new Schema({
+  organization: ObjectId,
+  name: String,
+});
+Game.index({name: 1, organization: 1});
+
+const Image = exports.Image = new Schema({
+    url: String
+})
+
+const Item = exports.Item = new Schema({
+  game: ObjectId,
+  slug: String,
+  type: String,
+  tags: [String],
+  meta: Mixed,
+  images: [Image],
+  folder: String,
+});
+Item.index({game: 1, slug: 1});
+
+const Folder = exports.folder = new Schema({
+  name: String
+});
+
+const Character = exports.Character = new Schema({
+  name: String,
+  inventory: String,
+})
+
+const Player = exports.Player = new Schema({
+  game: ObjectId,
+  organization: ObjectId,
+  username: {
+    type: String,
+  },
+  characters: [Character],
+  inventory: [String],
+});
+
+Player.index({"username": 1, "game": 1});
+Player.index({"game": 1, "characters.name": 1});
